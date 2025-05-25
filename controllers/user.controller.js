54be97f0dce65ad04d5495db0142e782a4d68f4b
Ken <jakobsenkenneth@hotmail.no>
2025-05-25 13:39:42 +0200

    added dashboard page and router

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,36 +1,30 @@
 const User= require('../lib/models/user.model');
 
 
-const createUser = async (req, res) => {
-    await User.create({
-        email:'kenneth@mail.com',
-        password:'password'
-    });
-    res.render('user',{message:'User created successfully', user:null})
-};
+const signup = async (req,res)=>{
+    const {email, password} = req.body;
+    const query = {email};
 
-const getUser = async (req, res) => {
-    const user = await User.findOne({email:'kenneth@mail.com'});
-    res.render('user',{
-        message:'User found successfully',
-        user:user
-    });
-}
-
-const deleteUser = async (req, res) => {
-    const user = await User.findOneAndDelete({email:'kenneth@mail.com'});
-    res.render('user',{
-        message:'User deleted successfully',
-        user:user
-    });
+    const existingUser = await User.findOne(query);
+    if(existingUser){
+        res.redirect('/signup')
+    }
+    else{
+        const hashedPassword = await bcrypt.hash(password,10)
+        const user = {
+            email,
+            password:hashedPassword,
+        };
+        const result = await User.create(user);
+        req.session.userId = result._id;
+        res.redirect('/dashboard');
+    }
 }
 
 
 
 
 module.exports = {
-    createUser,
-    getUser,
-    deleteUser
+   signup,
 };
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 const userRoute = require('./routes/user.route')
+const dashboardRouter = require('./routes/dashboard.route')
 
 require('dotenv').config();
 require('./lib/dbConnect');
@@ -12,6 +13,7 @@ app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}))
 app.use(
   session({
     secret:process.env.AUTH_SECRET,
@@ -20,19 +22,12 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index', { message: 'Hello from Node.js' });
-});
+app.use('/', userRoute);
+app.use((req,res)=>{
+  res.status(404).render('index',{message:'Not Found'})
+})
 
-app.get('/contact', (req, res) => {
-res.render('index', { message: 'The Contact Page' });
-});
-
-app.get('/about', (req, res) => {
-res.render('index', { message: 'The About Page' });
-});
-
-app.use('/users', userRoute);
+app.use('/dashboard',dashboardRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
