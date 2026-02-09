4f1a4a12bb4defee68d959e5f9e409ff66657db9
Kenneth J <jakobsenkenneth@hotmail.no>
2026-02-09 10:54:53 +0100

    x

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-const express = require('express');
-const session = require('express-session');
-const morgan = require('morgan');
-const flash = require('connect-flash');
-const {verifyUser} = require('./libs/middleware')
+const express = require("express");
+const session = require("express-session");
+const morgan = require("morgan");
+const flash = require("connect-flash");
+const { verifyUser } = require("./libs/middleware");
 const app = express();
-const PORT = 3000;
+const PORT = 10000;
 
-const userRoute = require('./routes/user.route')
-const dashboardRouter = require('./routes/dashboard.route')
+const userRoute = require("./routes/user.route");
+const dashboardRouter = require("./routes/dashboard.route");
 
-require('dotenv').config();
-require('./libs/dbConnect');
+require("dotenv").config();
+require("./libs/dbConnect");
 
-app.use(morgan('dev'));
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.use(morgan("dev"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-
-app.use(express.static('./public'));
-app.use(express.urlencoded({extended:false}))
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
-    secret:process.env.SESSION_SECRET,
-    saveUninitialized:true,
-    resave:false,
-  })
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: false,
+  }),
 );
 app.use(flash());
 
-app.use('/', userRoute);
-app.use('/dashboard',verifyUser, dashboardRouter);
+app.use("/", userRoute);
+app.use("/dashboard", verifyUser, dashboardRouter);
 
-
-
-app.use((req,res)=>{
-  res.status(404).render('./pages/index',{title:'Not Found',message:'No such page'})
-})
+app.use((req, res) => {
+  res
+    .status(404)
+    .render("./pages/index", { title: "Not Found", message: "No such page" });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
